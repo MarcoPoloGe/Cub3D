c8131a633f0571f5431047414496462a5959cd09
Marco Belarbi <mbelarbi@student.42lausanne.ch>
2022-07-14 14:15:20 +0200

    Added libft header and silenced minilibx;

--- src/cub3D.h
+++ src/cub3D.h
@@ -6,13 +6,14 @@
 /*   By: ktrosset <ktrosset@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/14 11:37:12 by ktrosset          #+#    #+#             */
-/*   Updated: 2022/07/14 11:38:19 by ktrosset         ###   ########.fr       */
+/*   Updated: 2022/07/14 14:09:53 by mbelarbi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
 
+# include "../libft/libft.h"
 # include <stdio.h>
 # include <math.h>
 

87586ac23e2a3b63b262031e323233b8406c2d9c
Marco Belarbi <mbelarbi@student.42lausanne.ch>
2022-08-17 15:11:15 +0200

    norm;

--- src/display_background.c
+++ src/display_background.c
@@ -3,7 +3,6 @@
 //dessine le top et bot en couleur
 void	render_background(t_img *frame, t_assets assets)
 {
-
 	int	y;
 	int	x;
 
@@ -13,7 +12,7 @@ void	render_background(t_img *frame, t_assets assets)
 		x = 0;
 		while (x < frame->width)
 		{
-			if(y < frame->height / 2)
+			if (y < frame->height / 2)
 				ft_put_pixel_frame(frame, y, x, encode_rgb(assets.f[0], assets.f[1], assets.f[2]));
 			else
 				ft_put_pixel_frame(frame, y, x, encode_rgb(assets.c[0], assets.c[1], assets.c[2]));
--- src/frame.c
+++ src/frame.c
@@ -2,11 +2,11 @@
 
 void	ft_put_pixel_frame(t_img *frame, int y, int x, int color)
 {
-	if(x < frame->width && y < frame->height && x > 0 && y > 0)
+	if (x < frame->width && y < frame->height && x > 0 && y > 0)
 		frame->addr[((y) * frame->width) + (x)] = color;
 }
 
-void ft_reset_frame(t_img *frame)
+void	ft_reset_frame(t_img *frame)
 {
 	int	y;
 	int	x;
@@ -24,7 +24,7 @@ void ft_reset_frame(t_img *frame)
 	}
 }
 
-void ft_push_frame(t_data *data)
+void	ft_push_frame(t_data *data)
 {
 	mlx_put_image_to_window(data->mlx, data->win, data->frame.ptr, 0, 0);
 	ft_reset_frame(&(data->frame));

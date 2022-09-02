17e16cf32a3e7212e0a8af87fce046c82d4d2d30
Chomosukee <keanu.trosset@hotmail.com>
2022-09-02 12:10:30 +0200

    arrow work for walking

--- src/move.c
+++ src/move.c
@@ -6,7 +6,7 @@
 /*   By: ktrosset <ktrosset@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/01 11:25:41 by ktrosset          #+#    #+#             */
-/*   Updated: 2022/09/01 14:07:04 by ktrosset         ###   ########.fr       */
+/*   Updated: 2022/09/02 11:56:49 by ktrosset         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,13 @@ void	ft_rotate_camera(t_camera *camera, double angle)
 		camera->dir_angle -= 360;
 }
 
-void	ft_move_player(double move, t_camera *camera)
+void	ft_move_player(t_data *data, double move, t_camera *camera)
 {
 	t_coord	next_coord;
 	t_coord	coord;
-
+	(void)data;
 	coord = camera->coord;
+	//if (coord.y + move)
 	next_coord.y = coord.y + move;
 	next_coord.x = coord.x;
 	next_coord = ft_rotate_point(coord, next_coord, camera->dir_angle);
@@ -41,14 +42,14 @@ int	keycode_handling(int keycode, t_data *data)
 	camera = &(data->camera);
 	if (keycode == 53)
 		leave(data, 0);
-	if (keycode == 0)
+	if (keycode == 0 || keycode == 123)
 		ft_rotate_camera(camera, -10);
-	if (keycode == 2)
+	if (keycode == 2 || keycode == 124)
 		ft_rotate_camera(camera, 10);
-	if (keycode == 1)
-		ft_move_player(0.5, camera);
-	if (keycode == 13)
-		ft_move_player(-0.5, camera);
+	if (keycode == 1 || keycode == 125)
+		ft_move_player(data, 0.5, camera);
+	if (keycode == 13 || keycode == 126)
+		ft_move_player(data, -0.5, camera);
 	i = -1;
 	return (0);
 }

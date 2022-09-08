7f5530c7ac3854fd7fe26249e058da8528281f5f
Marco Belarbi <mbelarbi@student.42lausanne.ch>
2022-09-08 14:53:51 +0200

    Added error for empty file parsing;

--- libft/perso_marco/ft_read_file.c
+++ libft/perso_marco/ft_read_file.c
@@ -12,6 +12,12 @@
 
 #include "perso_marco.h"
 
+char	*ft_read_file_error(char *file_name, char *error)
+{
+	ft_printf("Error: %s: %s\n", file_name, error);
+	return (NULL);
+}
+
 //permet de lire le fichier en entier et de verifier si le fichier est de la
 //bonne extension.
 //(si la valeur de ext est NULL alors toutes les extensions fonctionnent).
@@ -22,16 +28,10 @@ char	*ft_read_file(char *file_name, char *ext)
 	int		fd;
 
 	if (!ft_check_ext(file_name, ext))
-	{
-		ft_printf("Error: %s: %s\n", file_name, "incompatible extension");
-		return (NULL);
-	}
+		return (ft_read_file_error(file_name, "incompatible extension"));
 	fd = open(file_name, O_RDONLY);
 	if (fd == -1)
-	{
-		ft_printf("Error: %s: %s\n", file_name, strerror(errno));
-		return (NULL);
-	}
+		return (ft_read_file_error(file_name, strerror(errno)));
 	file = NULL;
 	line = ft_get_next_line(fd);
 	while (line != NULL)
@@ -41,5 +41,7 @@ char	*ft_read_file(char *file_name, char *ext)
 		line = ft_get_next_line(fd);
 	}
 	close(fd);
+	if (file == NULL)
+		return (ft_read_file_error(file_name, "input file is empty"));
 	return (file);
 }
